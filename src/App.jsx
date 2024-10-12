f19bb36cae2de4db522aea93d700e277ba7202bd
ugonnachidinmac <ugonnachidinma92@gmail.com>
2024-10-12 21:05:09 +0100

    testimony and footer work

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import Ourservices from './components/ourservices/Ourservices.jsx'
+import Footer from './components/ourservices/Footer/Footer.jsx'
+import Testimonial from './components/ourservices/Testimonial/Testimonial.jsx'
 import './App.css'
 
 function App() {
   
 
   return (
-   
-      
+      <>
+      <Testimonial />
       <Ourservices/>
- 
-
-
-   
+      <Footer />
+      </>
   )
 }
 
