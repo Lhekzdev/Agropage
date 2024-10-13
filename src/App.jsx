7c9b96dbdd5bc6b49b25f79ea6c9fcd264a1cdf4
unknown <huxaiphaibarheem@gmail.com>
2024-10-13 14:03:25 +0100

    Agropage

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Question from '../src/component/question/Question';
+
+function App() {
+  return (
+    
+      <Question />
+    
+  );
+}
+
+export default App;
